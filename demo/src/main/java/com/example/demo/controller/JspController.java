21aeb9bd463a1d82d9cf92aa2e5ecfeffa4130e3
chandrashekaradc407 <chandrashekaradc407@gmail.com>
2020-09-12 17:15:59 +0530

    added new lines

--- demo/src/main/java/com/example/demo/controller/JspController.java
+++ demo/src/main/java/com/example/demo/controller/JspController.java
@@ -5,5 +5,8 @@ public class JspController {
     public String  s =  "giri is great";
     private String mounika = "giri  X ";
     private String giri = "mounika loves giri and vise versa ";
+    private String giri1 = "mounika loves giri and vise versa ";
+    private String giri2 = "mounika loves giri and vise versa ";
+
 
 }
